7d344cd6344eb9d2ea307eda3b18b8d840339c4f
mouadlafnoune <mouad.lafnoune1@gmail.com>
2020-12-06 23:41:34 +0100

    fin security

--- src/main/java/com/VotreBanque/spring/security/SecurityConfig.java
+++ src/main/java/com/VotreBanque/spring/security/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.VotreBanque.spring.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.password.Md4PasswordEncoder;
 import org.springframework.security.core.userdetails.User;
 
 
@@ -13,15 +17,24 @@ import org.springframework.security.core.userdetails.User;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+	@Autowired
+	private DataSource dataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
-		@SuppressWarnings("deprecation")
-		  UserBuilder users = User.withDefaultPasswordEncoder();
-		  
-		  auth.inMemoryAuthentication()
-		  .withUser(users.username("admin").password("1234").roles("USER","ADMIN"))
-		  .withUser(users.username("user").password("1234").roles("USER"));
+//		@SuppressWarnings("deprecation")
+//		  UserBuilder users = User.withDefaultPasswordEncoder();
+//		  
+//		  auth.inMemoryAuthentication()
+//		  .withUser(users.username("admin").password("1234").roles("USER","ADMIN"))
+//		  .withUser(users.username("user").password("1234").roles("USER"));
+		
+		auth.jdbcAuthentication().dataSource(dataSource)
+		    .usersByUsernameQuery("select username as principal, password as credentials, active from users where username=?")
+		    .authoritiesByUsernameQuery("select username as principal, roles as role from users_roles where username=?")
+		    .rolePrefix("ROLE_")
+		    .passwordEncoder(new Md4PasswordEncoder());
 		 
 
 	}
